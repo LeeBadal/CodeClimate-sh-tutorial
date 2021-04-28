e988a87d5ece540abd8828f23e3447641f2a3f0c
LeeBadal <43146567+LeeBadal@users.noreply.github.com>
2021-04-28 18:33:07 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
-cDIR=$(pwd)
-DIR="/home/$USER/codeclimate-master"
-if [ -d "$DIR" ];
+
+if [ ! -f "/usr/local/bin/codeclimate" ]
 then
-  python3 tests/test.py
-  codeclimate analyze
-else
-  curl -L https://github.com/codeclimate/codeclimate/archive/master.tar.gz | tar xvz -C /home/$USER/
-  cd /home/$USER/codeclimate-* && sudo make install
-  cd $cDIR
-  codeclimate engines:install
-  python3 tests/test.py
-  codeclimate analyze
+	curl -L https://github.com/codeclimate/codeclimate/archive/master.tar.gz | tar xvz -C /home/$USER/
+	(cd /home/$USER/codeclimate-* && sudo make install)
 fi
 
+codeclimate engines:install
+python3 tests/test.py
+codeclimate analyze
